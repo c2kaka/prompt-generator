83eed5c90a239f9b8986a62b3fb684a8be43fc14
c2kaka <c2kaka@126.com>
2023-04-11 15:59:27 +0800

    feat: generate unit test prompt by config

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import {Form, Input, Button, Select} from 'antd';
+import './App.css'
+import {useState} from "react";
+
+const { TextArea } = Input;
+
+
+function App() {
+  const rolePrompt = 'You are a frontend developer.';
+
+  const providersPrompt = `Provide jasmine spy for all injected services.
+In the unit test, remember to add the following configurations:
+Use NO_ERRORS_SCHEMA from '@angular/core' in schemas.
+Import FormsModule from '@angular/forms'.
+Import MockModule from 'tdc-ui/mock'.
+Import MockModule from 'app/mock'.
+Import of and throwError from 'rxjs'.`;
+
+  const writeTestPromptOptions = [
+    `Write a unit test for the following component.`
+  ];
+
+  const coveragePromptOptions = [`Try to cover as much code and branches as possible.`];
+
+  const [form] = Form.useForm();
+
+  const generatePrompt = () => {
+    const role = form.getFieldValue('rolePrompt');
+    const providers = form.getFieldValue('providersPrompt');
+    const code = `Here is the code:
+${form.getFieldValue('code')}`;
+    const writeTestPrompt = form.getFieldValue('writeTestPrompt');
+    const coveragePrompt = form.getFieldValue('coveragePrompt');
+    const result = [role, providers, writeTestPrompt, coveragePrompt, code].filter(_item => _item != undefined).join('\n\n');
+    setResult(result);
+  };
+
+  const [result, setResult] = useState('');
+
+  return (
+    <>
+      <Form
+        className={"form"}
+        name="basic"
+        form={form}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ rolePrompt: rolePrompt, providersPrompt: providersPrompt }}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Role Prompt"
+          name="rolePrompt"
+        >
+          <Input disabled/>
+        </Form.Item>
+
+
+        <Form.Item
+          label="Providers Prompt"
+          name="providersPrompt"
+        >
+          <TextArea
+            style={{ height: 200 }}
+            showCount
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Write Test Prompt"
+          name="writeTestPrompt"
+        >
+          <Select
+            allowClear={true}
+            options={writeTestPromptOptions.map(_item => ({label: _item, value: _item}))}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Coverage Prompt"
+          name="coveragePrompt"
+        >
+          <Select
+            allowClear={true}
+            options={coveragePromptOptions.map(_item => ({label: _item, value: _item}))}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Code"
+          name="code"
+        >
+          <TextArea
+            showCount
+            style={{ height: 300 }}
+            className={"code-textarea"}
+          />
+        </Form.Item>
+      </Form>
+
+      <Button className={"submit-btn"} onClick={generatePrompt}>Submit</Button>
+
+      <TextArea
+        showCount
+        readOnly
+        className={"result-textarea"}
+        value={result}
+      />
+    </>
+  )
+}
+
+export default App
